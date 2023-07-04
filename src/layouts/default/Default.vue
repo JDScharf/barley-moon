c3f521e8ba7430c7853343917475444d9f39b9ee
Jason Scharf <43705075+JDScharf@users.noreply.github.com>
2023-07-04 17:53:19 -0400

    First commit of Liz's project

---
+++ src/layouts/default/Default.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <default-bar />
+
+    <default-view />
+  </v-app>
+</template>
+
+<script setup>
+  import DefaultBar from './AppBar.vue'
+  import DefaultView from './View.vue'
+</script>
+
+<style scoped>
+.v-application {
+  background-color: #211f1c;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <HelloWorld />
+  <!-- <Slider /> -->
+</template>
+
+<script setup>
+  import HelloWorld from '@/components/HelloWorld.vue'
+  // import Slider from '@/components/Slider.vue'
+</script>
